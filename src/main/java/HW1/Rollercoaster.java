329e718aa807459abae04f1dc23bce759e7bb894
JazminRondon <Jazmin_Rondon@outlook.com>
2022-09-24 12:14:08 -0400

    done

--- src/main/java/HW1/Rollercoaster.java
+++ src/main/java/HW1/Rollercoaster.java
@@ -3,7 +3,7 @@ package HW1;
 public class Rollercoaster implements Rideable {
     private final double tax =0.20 ;
     private int speed=130;
-git 
+
     @Override
     public int milesPerHour() {
         return speed;
